31866e86df922626b16de899d2a6d694f8636ca0
Renzo Johnson <4695400+renzojohnson@users.noreply.github.com>
2026-02-24 16:28:02 -0500

    Add file header docblocks with author info to all PHP files

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * Discord API
+ *
+ * @package   RenzoJohnson\Discord
+ * @author    Renzo Johnson <hello@renzojohnson.com>
+ * @copyright 2026 Renzo Johnson
+ * @license   MIT
+ * @link      https://renzojohnson.com
+ */
+
 declare(strict_types=1);
 
 namespace RenzoJohnson\Discord\Http;
